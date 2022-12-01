521496e43d3f52de5e580e16b4c342e51a365da8
renan <naneraruom@gmail.com>
2022-12-01 00:57:28 -0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+dotenv.config({ path: '.env.test' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API drivers')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('drivers')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
