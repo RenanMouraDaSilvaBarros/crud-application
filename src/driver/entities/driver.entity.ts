ce10cea8e8183eded841efb117ebbc76766f387f
renan <naneraruom@gmail.com>
2022-12-01 00:17:38 -0300

    feat: driver entity

---
+++ src/driver/entities/driver.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Driver {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+
+  @Column('text')
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column('text')
+  document: number;
+}
